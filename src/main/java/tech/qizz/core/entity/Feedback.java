e535792b12c0138c313bd605373093a47f08fde4
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-02-09 06:01:30 +0700

    feat: feedback create, update time column

--- src/main/java/tech/qizz/core/entity/Feedback.java
+++ src/main/java/tech/qizz/core/entity/Feedback.java
@@ -8,7 +8,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,4 +39,23 @@ public class Feedback {
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User user;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "modified_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date();
+        modifiedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        modifiedAt = new Date();
+    }
 }
