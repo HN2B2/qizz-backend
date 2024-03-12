8f0be4486938d112138b73b861df33a5a40d4a83
Thang Ngo <ngosythang123456@gmail.com>
2024-03-13 01:51:27 +0700

    feat: user reposibiliry

--- src/main/java/tech/qizz/core/manageNotifications/ManageNotificationsService.java
+++ src/main/java/tech/qizz/core/manageNotifications/ManageNotificationsService.java
@@ -1,4 +1,12 @@
 package tech.qizz.core.manageNotifications;
 
+
+import tech.qizz.core.entity.constant.NotificationTargetType;
+import tech.qizz.core.manageNotifications.dto.CreateNotificationRequest;
+import tech.qizz.core.manageNotifications.dto.GetAllNotificationsResponse;
+import tech.qizz.core.manageNotifications.dto.NotificationResponse;
+import tech.qizz.core.manageNotifications.dto.UpdateNotificationRequest;
+
 public interface ManageNotificationsService {
-}
+
+  }
