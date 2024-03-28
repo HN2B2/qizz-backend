3b64a34013b1e041d82be156717ed206ae6fbbe7
quynhnthe <quynhnthe171282@fpt.edu.vn>
2024-03-28 10:44:34 +0700

    feature: cloudinary image

---
+++ src/main/java/tech/qizz/core/util/CloudinaryImageUploadController.java
@@ -0,0 +1,25 @@
+package tech.qizz.core.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cloudinary")
+@RequiredArgsConstructor
+public class CloudinaryImageUploadController {
+    private final CloudinaryService cloudinaryService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<Map> uploadImage(@RequestParam("image") MultipartFile file){
+        Map data = this.cloudinaryService.upload(file);
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+}
